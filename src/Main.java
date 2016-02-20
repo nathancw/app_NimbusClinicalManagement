d80caf56533adf37ffc83e4a83b7088107c55b27
Nathaniel <nathanielcw@gmail.com>
2016-02-20 14:37:58 -0600

    Fixed the main.java code to get connection and query it. added sqljdbc driver to the classpath.

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,37 @@
-package database;
+//package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		getConnection();
+		Connection sqlconn = getConnection();
 		
+		Statement stmt = sqlconn.createStatement();
+		
+		String sqlQuery = "SELECT address from NCMSE.[NCM].[Patient_Test] where FirstName = 'Jason'";
+		
+		ResultSet rs = stmt.executeQuery(sqlQuery);
+		
+		//You have to add the rs.next or else it wont get the first row. I guess the result set points to the row before the first row? Normally they use while(rs.next()) 
+		if(rs.next()){
+		String address = rs.getString("address");
+		
+		System.out.println("Queried Patient_Test Table to get: " + address);
+		}
+		else
+			System.out.println("No Rows?");
 	}
 
 	public static Connection getConnection() throws Exception{
 		try {
-			String driver = "com.mysql.jdbc.Driver";
-			String url = "jdbc:mysql://ncmse.cnsk3lzn30dj.us-west-2.rds.amazonaws.com:3306/NCMSE";
+			
+			String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+			String url = "jdbc:sqlserver://ncmse.cnsk3lzn30dj.us-west-2.rds.amazonaws.com\\NCMSE:1433";
 			String user = "CSCE3513";
 			String password = "Nimbusdataserver";
 			Class.forName(driver);
